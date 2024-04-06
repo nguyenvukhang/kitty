99fab0de4bd93051d231a0ac07b39d83277c2feb
nguyenvukhang <brew4k@gmail.com>
2024-04-06 22:29:20 +0800

    Nerf sessions

--- alatty/session.py
+++ alatty/session.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
 # License: GPL v3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
-import os
-import shlex
-import sys
-from contextlib import suppress
-from functools import partial
-from typing import TYPE_CHECKING, Callable, Generator, Iterator, List, Mapping, Optional, Tuple, Union
+from typing import TYPE_CHECKING, Iterator, List, Optional, Tuple, Union
 
 from .cli_stub import CLIOptions
-from .layout.interface import all_layouts
 from .options.types import Options
-from .options.utils import resize_window, to_layout_names, window_size
 from .os_window_size import WindowSize, WindowSizeData, WindowSizes
 from .typing import SpecialWindowInstance
-from .utils import expandvars, log_error, resolve_custom_file, resolved_shell, shlex_split
+from .utils import resolved_shell
 
 if TYPE_CHECKING:
     from .launch import LaunchSpec
@@ -70,64 +63,8 @@ class Session:
             del self.tabs[-1]
         self.tabs.append(Tab(opts, name))
 
-    def set_next_title(self, title: str) -> None:
-        self.tabs[-1].next_title = title.strip()
-
-    def set_layout(self, val: str) -> None:
-        if val.partition(':')[0] not in all_layouts:
-            raise ValueError(f'{val} is not a valid layout')
-        self.tabs[-1].layout = val
-
-    def add_window(self, cmd: Union[None, str, List[str]], expand: Callable[[str], str] = lambda x: x) -> None:
-        from .launch import parse_launch_args
-        needs_expandvars = False
-        if isinstance(cmd, str):
-            needs_expandvars = True
-            cmd = list(shlex_split(cmd))
-        spec = parse_launch_args(cmd)
-        if needs_expandvars:
-            assert isinstance(cmd, list)
-            limit = len(cmd)
-            if len(spec.args):
-                with suppress(ValueError):
-                    limit = cmd.index(spec.args[0])
-            cmd = [(expand(x) if i < limit else x) for i, x in enumerate(cmd)]
-            spec = parse_launch_args(cmd)
-
-        t = self.tabs[-1]
-        if t.next_title and not spec.opts.window_title:
-            spec.opts.window_title = t.next_title
-        spec.opts.cwd = spec.opts.cwd or t.cwd
-        t.windows.append(WindowSpec(spec))
-        t.next_title = None
-        if t.pending_resize_spec is not None:
-            t.windows[-1].resize_spec = t.pending_resize_spec
-            t.pending_resize_spec = None
-
-    def resize_window(self, args: List[str]) -> None:
-        s = resize_window('resize_window', shlex.join(args))[1]
-        spec: ResizeSpec = s[0], s[1]
-        t = self.tabs[-1]
-        if t.windows:
-            t.windows[-1].resize_spec = spec
-        else:
-            t.pending_resize_spec = spec
-
     def add_special_window(self, sw: 'SpecialWindowInstance') -> None:
         self.tabs[-1].windows.append(WindowSpec(sw))
-
-    def focus(self) -> None:
-        self.active_tab_idx = max(0, len(self.tabs) - 1)
-        self.tabs[-1].active_window_idx = max(0, len(self.tabs[-1].windows) - 1)
-
-    def set_enabled_layouts(self, raw: str) -> None:
-        self.tabs[-1].enabled_layouts = to_layout_names(raw)
-        if self.tabs[-1].layout not in self.tabs[-1].enabled_layouts:
-            self.tabs[-1].layout = self.tabs[-1].enabled_layouts[0]
-
-    def set_cwd(self, val: str) -> None:
-        self.tabs[-1].cwd = val
-
 
 def create_sessions(
     opts: Options,
